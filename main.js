b34ef42f3dc9b519e8b58e4ec10753025d044f2d
gabrielsz00 <104466992+gabrielsz00@users.noreply.github.com>
2022-10-11 18:32:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,65 @@
-var canvas = new fabric.Canvas("myCanvas");
- var x = document.getElementById("myAudio");
 
-function newImage()
+canvas = new fabric.Canvas("myCanvas")
+ blockY=1;
+ blockX=1;
+
+blockImageWidth = 350;
+blockImageHeight = 430;
+
+var blockImageObject= "";
+
+function newImage(getImage)
 {
-	
-	
+	fabric.Image.fromURL(getImage, function (img){
+		blockImageObject = img
+		blockImageObject.scaleToWidth(blockImageWidth)
+		blockImageObject.scaleToHeight(blockImageHeight)
+		blockImageObject.set({top: blockY, left: blockX})
+		canvas.add(blockImageObject)
+
+
+	})
 }
 
-function playSound(){
-	x.play();
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // adicione os códigos adequados às teclas
+	{
+		blockX = 100;
+		newImage('rr1.png')
+		console.log("R")
+	}
+	if(keyPressed == '86')
+	{
+		blockX = -200;
+		newImage('br.png')
+		console.log("V")
+	}
+	
+	if(keyPressed == '65')
+	{
+		blockX =300;
+		newImage('yr.png')
+		console.log("A")
+	}
+	
+	if(keyPressed == '73')
+	{
+		blockX = 600;
+		newImage('pr.png')
+		console.log("I")
+	}
+
+	if(keyPressed == '69')
+	{
+		blockX = 800;
+		newImage('gr.png')
+		console.log("E")
+	}
+	
 }
