705575a1e8d6000b54fd9f3865cb1e097d2762b6
gabrielsz00 <104466992+gabrielsz00@users.noreply.github.com>
2022-10-25 18:33:54 -0300

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+
+function addUser(){
+    player1name = document.getElementById("player1").value
+    player2name = document.getElementById("player2").value
+    localStorage.setItem("player1name", player1name)
+    localStorage.setItem("player2name", player2name)
+    window.location = "game_page.html"
+}
+
+
+
+
