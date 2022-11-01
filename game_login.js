b12ec87d026c077f0aa85db07d142191dc631e77
gabrielsz00 <104466992+gabrielsz00@users.noreply.github.com>
2022-11-01 18:37:02 -0300

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -2,10 +2,11 @@
 function addUser(){
     player1name = document.getElementById("player1").value
     player2name = document.getElementById("player2").value
-    localStorage.setItem("player1name", player1name)
-    localStorage.setItem("player2name", player2name)
+    localStorage.setItem("player1Name", player1name)
+    localStorage.setItem("player2Name", player2name)
     window.location = "game_page.html"
 }
+
 
 
 
---
+++ game_page.js
@@ -0,0 +1,28 @@
+
+	player1Name = localStorage.getItem("player1Name");
+	player2Name = localStorage.getItem("player2Name");
+
+	player1Score = 0;
+	player2Score = 0;
+
+document.getElementById("player1Name").innerHTML = player1Name + " : ";
+document.getElementById("player2Name").innerHTML = player2Name + " : ";
+
+document.getElementById("player1Score").innerHTML = player1Score ;
+document.getElementById("player2Score").innerHTML = player2Score ;
+
+
+function send() {
+
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt (number1) * parseInt(number2);
+
+    question_number = "<h4>" + number1 + " x "+ number2 +"</h4>";
+    input_box = "<br>Resposta : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check() ' >Checar</button>"
+    row = question_number + input_box + check_button ;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
