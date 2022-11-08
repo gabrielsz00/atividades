3cead4b15e0d141109e23b48633d81c4c0aeb507
gabrielsz00 <104466992+gabrielsz00@users.noreply.github.com>
2022-11-08 18:41:34 -0300

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -26,3 +26,48 @@ function send() {
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
 }
+
+questionTurn = "player1"
+answerTurn = "player2"
+
+function check(){
+	getAnswer = document.getElementById("input_check_box").value
+	answer = getAnswer.toLowerCase()
+	if (answer == actual_answer)
+	{
+		if (answerTurn == "player1")
+		{
+		player1Score = player1Score + 1
+		document.getElementById("player1Score").innerHTML = player1Score;
+		}
+		else
+		{
+			player2Score = player2Score + 1;
+		document.getElementById("player2Score").innerHTML = player2Score;
+		}
+	}
+		
+
+	if (questionTurn == "player1")
+	{
+	questionTurn = "player2"
+	document.getElementById("playerQuestion").innerHTML = "Turno da pergunta:" + player2Name
+	}
+	else{
+		questionTurn = "player1"
+		document.getElementById("playerQuestion").innerHTML = "Turno da pergunta:" + player1Name	
+	}
+
+	if (answerTurn == "player1"){
+		answerTurn = "player2"
+		document.getElementById("playerAnswer").innerHTML = "Turno da resposta:" + player2Name
+		}
+		else{
+			answerTurn = "player1"
+			document.getElementById("playerAnswer").innerHTML = "Turno da resposta:" + player1Name	
+		}
+		document.getElementById("output").innerHTML = ""
+}
+
+
+
