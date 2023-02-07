8d0092b1091f9fdef0349fd05baf4f8e2fe8d0f7
gabrielsz00 <104466992+gabrielsz00@users.noreply.github.com>
2023-02-07 18:49:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,78 +1,17 @@
-var SpeechRecognition = window.webkitSpeechRecognition;
-var Content;
-var recognition = new SpeechRecognition();
-
-function start()
-{
-    recognition.start();
-} 
-
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90,
+})
 
 camera = document.getElementById("camera");
-Webcam.set({
-    width:500,
-    height:400,
-    image_format : 'jpeg',
-    jpeg_quality:90
-});
+Webcam.attach('#camera')
 
-
-
-function speak(){
-
-    
-    var synth = window.speechSynthesis;
-    Webcam.attach(camera);
-
-    speakData = "Tirando sua selfie em 5 segundos";
-    var utterThis = new SpeechSynthesisUtterance(speakData);
-    synth.speak(utterThis);
-
-    setTimeout(function()
-    {
-    imgId = "selfie1";
-    takeSelfie();
-    
-    speakData = "Tirando sua selfie em 5 segundos";
-    var utterThis = new SpeechSynthesisUtterance(speakData) ;
-    synth.speak(utterThis);
-    }, 5000) ;
-
-    setTimeout(function()
-    {
-    imgId = "selfie2";
-    takeSelfie();
-    
-    speakData = "Tirando sua selfie em 10 segundos";
-    var utterThis = new SpeechSynthesisUtterance(speakData) ;
-    synth.speak(utterThis);
-    }, 10000) ;
-
-    setTimeout(function()
-    {
-    imgId = "selfie3";
-    takeSelfie();
-    
-    speakData = "Tirando sua selfie em 15 segundos";
-    var utterThis = new SpeechSynthesisUtterance(speakData) ;
-    synth.speak(utterThis);
-    }, 15000) ;
-       
-
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+ data_uri + '">'
+    })
 }
 
-function takeSelfie(){
-console.log(imgId);
-
-Webcam.snap(function(data_uri){
-if(imgId=="selfie1"){
-document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>';
-}
-if(imgId=="selfie2"){
-    document.getElementById("result2").innerHTML = '<img id="selfie2" src="'+data_uri+'"/>';
-    }
-if(imgId=="selfie3"){
-        document.getElementById("result3").innerHTML = '<img id="selfie3" src="'+data_uri+'"/>';
-        }    
-    });
-}
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/UOtSP14hU/', modelLoaded)
