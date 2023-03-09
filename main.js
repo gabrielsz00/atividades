c670202fbe003339f562611ec37dccc544c87df2
gabrielsz00 <104466992+gabrielsz00@users.noreply.github.com>
2023-03-09 18:55:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-https://teachablemachine.withgoogle.com/models/weLF_W5df/
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true})
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/weLF_W5df/model.json', modelReady)
+}
+
+function modelReady(){
+    classifier.classify(gotResults)
+}
+
+function gotResults(error, results){
+    console.log('got result')
+}
