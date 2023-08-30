5f22f587e3f2cb9fb7601bf868b90f27d3c1b28a
gabrielsz00 <104466992+gabrielsz00@users.noreply.github.com>
2023-08-30 18:50:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,59 +1,25 @@
-song1 = ""
-song2 = ""
-
-scorerigthwrist = 0
-scoreleftwrist = 0
-
-rightwristx = 0
-rightwristy = 0
-
-leftwristx = 0
-leftwristy = 0
+img = ""
+objects = []
+modelstatus =  "";
 
 function preload(){
-    song1 = loadSound("chão.mp3")
-    song2 = loadSound("Undertale.mp3")
+    img = loadImage('cachorro.jpg')
 }
 
 function setup(){
-    canvas = createCanvas(600, 500)
+    canvas = createCanvas(600, 420)
     canvas.center();
-
-    video = createCapture(VIDEO)
-
-
-    poseNet = ml5.poseNet(video, modelLoaded)
-    poseNet.on('pose', gotPoses)
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando objetos..."
+    
 }
 
 function modelLoaded(){
     console.log("Modelo carregado!")
+    modelstatus = true 
+    objectDetector.detect(img, gotResult)
 }
 
-function draw(){
-    image(video, 0, 0, 600, 500)
-}
-
-function play(){
-    song.play()
-    song.rate(1)
-    song.setVolume(1)
-}
-
-function gotPoses(results){
-    if(results.length > 0){
-console.log(results)
-scorerigthwrist = results[0].pose.keypoints[10].score
-scoreleftwrist = results[0].pose.keypoints[9].score
-leftwristx = results[0].pose.leftWrist.x    
-leftwristy = results[0].pose.leftWrist.y
-
-rightwristx = results[0].pose.rightWrist.x    
-rightwristy = results[0].pose.rightWrist.y  
-
-console.log(leftwristx, leftwristy)
-console.log(rightwristx, rightwristy)
-console.log(scorerigthwrist, scoreleftwrist)
-
-}
+function click1(){
+window.location.href = './index1.html'
 }
