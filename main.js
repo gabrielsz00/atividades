dd45a05c4d13c5a56912c00e0a99f9d59333fba8
gabrielsz00 <104466992+gabrielsz00@users.noreply.github.com>
2023-10-04 18:44:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function setup(){
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded)
     document.getElementById("status").innerHTML = "Status: Detectando objetos..."
+    var texto = document.getElementById("texto").value
 }
 
 function modelLoaded(){
@@ -22,4 +23,34 @@ function modelLoaded(){
     objectDetector.detect(video, gotResults)
 }
 
-function gotResults(){}
+function gotResults(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results)
+    objects = results
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380)
+    if (status != ""){
+
+
+
+
+        objectDetector.detect(video, gotResults)
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objeto detectado!"
+            document.getElementById("numberOfObjects").innerHTML = "Quantidade de objetos detectados :" + objects.length;
+        fill("#FF0000")
+            percent = floor(objects[i].confidence*100)
+            text(objects[i].label + " " + percent + "%", objects[i].x +15,objects[i].y +15)
+            noFill()
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+    else{
+        document.getElementById("encontrado").innerHTML = texto
+    }
+}
