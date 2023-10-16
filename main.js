0863d3eed25b7a758b2b76debc0c13da25613fab
gabrielsz00 <104466992+gabrielsz00@users.noreply.github.com>
2023-10-16 18:47:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,56 +1,174 @@
-objects = []
-status= ""
+paddle1 = 10;
+paddle2 = 10;
 
+paddle1X = 10;
+
+paddle1Y = '';
+paddle2Y = 685;
+
+paddle1Height = 110;
+paddle2Height = 70;
+
+score1 = 0;
+score2 = 0;
+
+
+playerscore = 0;
+pcscore = 0;
+
+audio1 = '';
+
+
+//Coordenadas x, y, raio, velocidade em x e velocidade em y
+ball = {
+    x:350/2,
+    y:480/2,
+    r:20,
+    dx:3,
+    dy:3
+}
 
 function setup(){
-    canvas = createCanvas(480, 380);
-    canvas.center();
-    video = createCapture(VIDEO)   
-    video.size(480, 380)
-    video.hide()
-    video.center();
+  canvas =  createCanvas(700,550);
 }
 
-function start(){
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
-    document.getElementById("status").innerHTML = "Status: Detectando objetos..."
-    var texto = document.getElementById("texto").value
-}
-
-function modelLoaded(){
-    console.log("Modelo carregado!")
-    status = true
-    objectDetector.detect(video, gotResults)
-}
-
-function gotResults(error, results){
-    if (error){
-        console.log(error);
-    }
-    console.log(results)
-    objects = results
-}
 
 function draw(){
-    image(video, 0, 0, 480, 380)
-    if (status != ""){
+
+  background(0); 
+
+  fill("black");
+  stroke("black");
+  rect(680,0,20,700);
+
+  fill("black");
+  stroke("black");
+  rect(0,0,20,700);
+
+  //Chamar a função paddleInCanvas() 
+  paddleInCanvas();
+
+  //Raquete do jogador
+  fill(250,0,0);
+  stroke(0,0,250);
+  strokeWeight(0.5);
+  paddle1Y = mouseY; 
+  rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+
+
+  //Raquete do computador
+  fill("#FFA500");
+  stroke("#FFA500");
+  paddle2y =ball.y-paddle2Height/2;  
+  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+
+  //Chamar a função midline()
+  midline();
+
+  //Chamar a função drawScore() 
+  drawScore();
+
+  //Chamar a função models()  
+  models();
+
+  //Chamar a função move() (muito importante para o jogo)
+  move();
+}
 
 
 
+//Função reset() para quando a bola não colidir com a raquete
+function reset(){
+   ball.x = width/2+100,
+   ball.y = height/2+100;
+   ball.dx=3;
+   ball.dy =3;
+   
+}
 
-        objectDetector.detect(video, gotResults)
-        for(i=0; i<objects.length; i++){
-            document.getElementById("status").innerHTML = "Status: Objeto detectado!"
-            document.getElementById("numberOfObjects").innerHTML = "Quantidade de objetos detectados :" + objects.length;
-        fill("#FF0000")
-            percent = floor(objects[i].confidence*100)
-            text(objects[i].label + " " + percent + "%", objects[i].x +15,objects[i].y +15)
-            noFill()
-            stroke("#FF0000");
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
-        }
+
+//Função midline() para desenhar uma linha no centro do canvas
+function midline(){
+    for(i=0;i<480;i+=10) {
+    y = 0;
+    fill("white");
+    stroke(0);
+    rect(width/2,y+i,10,480);
     }
-    else{
-        document.getElementById("encontrado").innerHTML = texto
-    }
+}
+
+
+//Funçao drawScore() para mostra o placar
+function drawScore(){
+    textAlign(CENTER);
+    textSize(20);
+    fill("red");
+    stroke(250,0,0)
+    text("Jogador:",100,50)
+    text(playerscore,180,50);
+    text("Computador:",500,50)
+    text(pcscore,595,50)
+}
+
+
+//Função muito importante para o jogo
+function move(){
+   fill(50,350,0);
+   stroke(255,0,0);
+   strokeWeight(0.5);
+   ellipse(ball.x,ball.y,ball.r,20)
+   ball.x = ball.x + ball.dx;
+   ball.y = ball.y + ball.dy;
+   if(ball.x+ball.r>width-ball.r/2){
+       ball.dx=-ball.dx-0.5;       
+   }
+  if (ball.x-2.5*ball.r/2< 0){
+  if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
+    ball.dx = -ball.dx+0.5;
+    playerscore++;
+  }
+  else{
+    pcscore++;
+    reset();
+    navigator.vibrate(100);
+  }
+}
+
+if(pcscore ==4){
+    fill("#FFA500");
+    stroke(0)
+    rect(0,0,width,height-1);
+    fill("white");
+    stroke("white");
+    textSize(25)
+    text("Game Over!☹☹",width/2,height/2);
+    text("Recarregue a página!",width/2,height/2+30)
+    noLoop();
+    pcscore = 0;
+}
+   if(ball.y+ball.r > height || ball.y-ball.r <0){
+       ball.dy =- ball.dy;
+   }   
+}
+
+
+//Largura e altura do canvas e velocidade da bola 
+function models(){
+    textSize(18);
+    fill('red');
+    noStroke();
+    text("Largura:"+width,195,15);
+    text("Velocidade:"+abs(ball.dx),65,15);
+    text("Altura:"+height,300,15)
+}
+
+
+//Esta função ajuda a evitar que a raquete saia do canvas
+function paddleInCanvas(){
+  if(mouseY+paddle1Height > height){
+    mouseY=height-paddle1Height;
+  }
+  if(mouseY < 0){
+    mouseY =0;
+  }  
 }
